cd612d7fddceaabe5e5f2f03b21711489c9ac31c
s24285 <92219284+s24285@users.noreply.github.com>
2022-03-31 06:16:43 +0200

    Create robociki.java

---
+++ zjazd2/robociki.java
@@ -0,0 +1,13 @@
+package gamers;
+
+import main.Pair;
+import main.Unit;
+
+@SuppressWarnings("serial")
+public class s24285 extends Unit {
+
+    public s24285(final String id, final Pair position, final double rotate,
+                  final CollisionModel model) {
+        super(id, position, rotate, model);
+        // TODO
+    }
