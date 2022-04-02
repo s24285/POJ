0136052ce14c7fdd5a745fb56ec9aa447dcf49df
s24285 <92219284+s24285@users.noreply.github.com>
2022-04-02 13:18:16 +0200

    Create Prostokat.java

---
+++ kolokwium3/Prostokat.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Prostokat {
+    private double bok1;
+    private double bok2;
+
+    public Prostokat (double bok1, double bok2) {
+        this.bok1 = bok1;
+        this.bok2 = bok2;
+    }
+
+    public Prostokat() {
+    }
+
+    public void setBok1 (double bok1) {
+        this.bok1 = bok1;
+    }
+
+    public void setBok2 (double bok2) {
+        this.bok2 = bok2;
+    }
+
+    public double getBok1 () {
+        return this.bok1;
+    }
+
+    public double getBok2 () {
+        return this.bok2 = bok2;
+    }
+
+    public double przekatna (double param1) {
+        return (Math.sqrt((bok1 * bok1) + (bok2 * bok2)) * 2.5);
+    }
+
+    public double przekatna () {
+        return Math.sqrt((bok1 * bok1) + (bok2 * bok2));
+    }
+
+    public int przekatna (int param2) {
+        return (int) (bok1 * Math.sqrt(2) * 2);
+    }
+
+}
