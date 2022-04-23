7833a762f69655290659b9b70375ff90ca62a8b9
s24285 <92219284+s24285@users.noreply.github.com>
2022-04-23 13:39:53 +0200

    Create Buzka.java

---
+++ kolokwium4/Buzka.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Buzka implements PrzedstawiaEmocje, Rysowalny, Skalowalny {
+
+    char c;
+
+    public Buzka(char c) {
+    }
+
+
+    public void draw() {
+        System.out.println(":-" + c);
+    }
+
+    @Override
+    public boolean czySieUsmiecha() {
+        return true;
+    }
+
+    @Override
+    public void przestanSieUsmiechac() {
+        System.out.println(c);;
+    }
+
+    public void draw(int i) {
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        return true;
+    }
+
+}
